a55eb43bc891f2b9a762aaeccf7e9e6ad38949c7
jam1garner <jam1.mcleod@hotmail.com>
2020-04-18 23:45:27 -0400

    Add thread examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![feature(proc_macro_hygiene)]
 
 use skyline::{hook, install_hook};
+use std::thread;
+use std::time::Duration;
 
 extern "C" fn test() -> u32 {
     2
@@ -27,10 +29,39 @@ pub fn main() {
 
     let x = test();
 
+    // Make a vector to hold the children which are spawned.
+    let mut children = vec![];
+
+    let sleep_times = [0, 100, 500, 200, 5000, 100, 150, 0, 0, 300];
+
+    for i in 0..10 {
+        let sleep_time = sleep_times[i];
+        // Spin up another thread
+        children.push(thread::spawn(move || {
+            thread::sleep(Duration::from_millis(sleep_time));
+            println!("this is thread number {}", i);
+        }));
+    }
+
+    for child in children {
+        // Wait for the thread to finish. Returns a result.
+        let _ = child.join();
+    }
+
     println!("[main] test returned: {}", x); // 3
 
     println!("{}", std::fs::read_to_string("sd:/test.txt").unwrap());
     for x in std::fs::read_dir("sd:/atmosphere").unwrap() {
         println!("{:?}", x.unwrap());
     }
+
+    // keep-alive thread
+    thread::spawn(||{
+        loop {
+            println!("Still alive?");
+            thread::sleep(Duration::from_secs(3));
+            println!("Still alive!");
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
 }
