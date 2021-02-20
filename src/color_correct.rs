fc8a7005721a9e33f1b75605bf4b671941d8d1a0
itsmeft24 <57544858+itsmeft24@users.noreply.github.com>
2021-02-19 20:25:57 -0500

    Update color_correct.rs

--- src/color_correct.rs
+++ src/color_correct.rs
@@ -9,7 +9,7 @@ pub fn color_correct(skin_data: &mut image::RgbaImage) {
                 let pixel = &mut channels[i];
                 
                 // gamma brightening by a factor of 1.385, bounded to [0, 184]
-                *pixel = ((((*pixel as f64) / 255.0).powf(1.0f64 / 1.385f64) * 255.0) * 184.0 / 255.0) as u8;
+                *pixel = ((((*pixel as f64) / 255.0).powf(1.0f64 / 1.200f64) * 255.0) * 191.0 / 255.0) as u8;
             }
         }
     }
